7dfd025bb1905765411c965fdd2238230a4d1306
Jessica Stockdale <jessica_stockdale@sfu.ca>
2022-02-08 09:20:18 -0800

    penalized maximum likelihood

--- analysis-new/likelihood_func.R
+++ analysis-new/likelihood_func.R
@@ -51,5 +51,39 @@ func_loglik_2 <- function (par,dat_omic,parameters) {
 
 
 
+# Penalized maximum likelihood: 
+penalized_negbin.loglik <- function (params,times, test_prop, pen.weight, penalties) {
+  x <-   as.data.frame(deSolve::ode(y=init,time=times,func= sveirs,
+                                    parms= params)) 
+  prediction <-  params["p"]*params["sigma"]*(x$Er+ x$Erv + x$Erw
+                                              + x$Em+ x$Emv + x$Emw)*test_prop[1:nrow(x)]
+  ll <- sum(dnbinom(x=dat_omic$value,
+              mu=prediction,size=1/params["theta"],
+              log=TRUE))
+  # Calculate squared difference between known_prop = 0.5 and proportion resident strain on date_known_prop = Dec 12
+  pen <- (((x$Er+x$Erv+x$Erw)/(x$Er+x$Erv+x$Erw + x$Em+x$Emv+x$Emw))[penalties$date_known_prop] - penalties$known_prop)^2
+  # Calculate squared difference between known_growth = 0.2 and mutant relative growth rate during period_known_growth
+  pen2 <- (get_growth_rate(x, startoffset = penalties$period_known_growth[1], 
+           duration = penalties$period_known_growth[2]-penalties$period_known_growth[1])$mutrate - penalties$known_growth)^2
+  return(ll - (pen+pen2)*pen.weight/2)
+}
+func_penloglik <- function (par,test_prop,dat_omic,parameters, pen.size, penalties) {
+  
+  # 'par' contains the parameters to be fit. Replace their default value in 'parameters'
+  if(!all(names(par) %in% names(parameters))) stop('Names of pars to fit do not match names in parameters object')
+  parameters[names(par)] <- par
+  
+  # Convert dates to numbered days
+  times <- 1:nrow(dat_omic)
+  penalties$date_known_prop <- dat_omic$day[dat_omic$date==penalties$date_known_prop]
+  penalties$period_known_growth <- c(dat_omic$day[dat_omic$date==period_known_growth[1]],dat_omic$day[dat_omic$date==period_known_growth[2]])
+  
+  # Penalty weight calculated as relative to negloglh at initial values
+  pen.weight <- -pen.size*negbin.loglik(parameters,times,test_prop) 
+  
+  -penalized_negbin.loglik(params = parameters, times =  times, test_prop, pen.weight=pen.weight, penalties = penalties)
+}
+
+
 
 
