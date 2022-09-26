8508d289c96828a49fcffb07035379d816b9d46d
carolinecolijn <ccolijn@gmail.com>
2022-09-25 23:08:29 -0700

    modified booster script

--- analysis-new/pipps_boosters.R
+++ analysis-new/pipps_boosters.R
@@ -1,10 +1,12 @@
 
 # This script follows on directly from pipps_projection.r
-load("projectionscript_out.Rdata")
+load("projectionscript_out_JS.Rdata")
 
 # Adjust dates for projection
 old_intro_date  = intro_date # Keep track of 'day 0'
-intro_date <-   ymd("2022-07-01")
+intro_date <-   ymd("2022-06-20") # NOTE MOVED TO JUNE 
+proj_out = filter(proj_out, date<= intro_date) # sets last part of trajectory to the intro date to get the last state, for the IC of the new run 
+initscale=1.5
 stop_date <- intro_date + 300 # 300 days projection
 times = as.numeric(intro_date - old_intro_date):(as.numeric(stop_date - old_intro_date)) # Keep the same time count going
 
@@ -25,7 +27,7 @@ IHRcdn = 0.00484 # note: Canada-wide IHR
 
 #### 1. BASELINE Omicron scenario 
 params_newmutant = list("beta_r" = rem_parameters["beta_r"]*1.15,
-                        "beta_m" = rem_parameters["beta_m"]*1.22,
+                        "beta_m" = rem_parameters["beta_m"]*1.18,
                         "epsilon_m" = (1-0.325), # Previous mutant epsilon was 0.6
                         "epsilon_r" = (1-0.325),
                         "c_m" = rem_parameters["c_m"]*1.0,
@@ -51,7 +53,7 @@ x =new_model$init_newm
 pie(as.numeric(x),  
     labels = colnames(out_samp)[2:ncol(out_samp)], radius = 1.0)
 
-newstate = tweak_init(oldstate=x, myfactor=2) 
+newstate = tweak_init(oldstate=x, myfactor=initscale) 
 pie(as.numeric(newstate),  
     labels = colnames(out_samp)[2:ncol(out_samp)], radius = 1.0)
 
@@ -82,7 +84,7 @@ baseline.df = fproj_out %>% dplyr::select(date, Total) %>%
 
 #### 2. MORE BOOSTING than the baseline scenario. raise beffm i think. 
 params_newmutant = list("beta_r" = rem_parameters["beta_r"]*1.15,
-                        "beta_m" = rem_parameters["beta_m"]*1.22,
+                        "beta_m" = rem_parameters["beta_m"]*1.18,
                         "epsilon_m" = (1-0.325), # Previous mutant epsilon was 0.6
                         "epsilon_r" = (1-0.325),
                         "c_m" = rem_parameters["c_m"]*1.0,
@@ -108,7 +110,7 @@ x =new_model$init_newm
 pie(as.numeric(x),  
     labels = colnames(out_samp)[2:ncol(out_samp)], radius = 1.0)
 
-newstate = tweak_init(oldstate=x, myfactor=2) 
+newstate = tweak_init(oldstate=x,  myfactor=initscale) 
 pie(as.numeric(newstate),  
     labels = colnames(out_samp)[2:ncol(out_samp)], radius = 1.0)
 
@@ -140,7 +142,7 @@ moreboost.df = fproj_out %>% dplyr::select(date, Total) %>%
 
 #### 2. LESS BOOSTING than the baseline scenario. raise beffm i think. 
 params_newmutant = list("beta_r" = rem_parameters["beta_r"]*1.15,
-                        "beta_m" = rem_parameters["beta_m"]*1.22,
+                        "beta_m" = rem_parameters["beta_m"]*1.18,
                         "epsilon_m" = (1-0.325), # Previous mutant epsilon was 0.6
                         "epsilon_r" = (1-0.325),
                         "c_m" = rem_parameters["c_m"]*1.0,
@@ -166,7 +168,7 @@ x =new_model$init_newm
 pie(as.numeric(x),  
     labels = colnames(out_samp)[2:ncol(out_samp)], radius = 1.0)
 
-newstate = tweak_init(oldstate=x, myfactor=2) 
+newstate = tweak_init(oldstate=x,  myfactor=initscale) 
 pie(as.numeric(newstate),  
     labels = colnames(out_samp)[2:ncol(out_samp)], radius = 1.0)
 
@@ -205,18 +207,20 @@ alldf = rbind(baseline.df, moreboost.df, lessboost.df)
 # incident infections in Canada 
 ggplot(filter(alldf, date <= maxdate), aes(x=date, y=Total, fill=Scenario))+
   geom_ribbon(aes(x=date, ymin = 0.75*(38/5)*(100000/38e6)*Total, ymax=1.25*(38/5)*(100000/38e6)*Total), 
-               alpha=0.3)+  scale_fill_brewer(palette="Paired")+ xlab("")+
+               alpha=0.45)+  scale_fill_brewer(palette="Set1")+ xlab("")+
   ylab("Daily new infections per 100K") + theme_minimal()
+ggsave("boost-scenarios-infections.pdf") 
 
 # total hosps in Canada based on an 8 day stay and the IHR from public data, above
 ggplot(filter(alldf, date <= maxdate), aes(x=date, y=Total, fill=Scenario))+
   geom_ribbon(aes(x=date, ymin = IHRcdn*8*0.75*(38/5)*Total, 
                   ymax=IHRcdn*8*1.25*(38/5)*Total), 
-              alpha=0.3)+  scale_fill_brewer(palette="Paired")+ xlab("")+
+              alpha=0.45)+  scale_fill_brewer(palette="Set1")+ xlab("")+
   ylab("Total hospitalizations") + 
   geom_point(data=filter(hosp, Date > min(fproj_out$date-250)), 
-             aes(x=Date,y=COVID_HOSP),inherit.aes = F)+xlab("Date") + theme_minimal()
+             aes(x=Date,y=COVID_HOSP),inherit.aes = F)+xlab("") + theme_minimal()
 theme_minimal()
+ggsave("boost-scenarios-hosp.pdf") 
 
 
 # plots of one projection at a time
